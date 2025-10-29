d42108465a82051147a0bc43c1332864685dc3dc
TrucKKun0 <zanpakutobleach366@gmail.com>
2025-10-29 12:18:42 +0545

    Add refresh token and logout functionality to identity service

--- identity-services/controllers/identity-controller.js
+++ identity-services/controllers/identity-controller.js
@@ -1,7 +1,8 @@
 const logger = require("../utils/loggers");
-const { validateRegistration } = require("../utils/validation");
+const { validateRegistration,validatelogin } = require("../utils/validation");
 const User = require("../models/users");
 const { generateToken } = require("../utils/generateToken");
+const RefreshToken = require("../models/refreshToken");
 
 //user registration
 const registerUser = async (req, res) => {
@@ -60,6 +61,7 @@ const loginUser = async (req,res)=>{
     }
     const {email, password} = req.body;
     let user = await User.findOne({email});
+    logger.info("User found", user._id);
     if(!user){
       logger.warn("User not found", email);
       return res.status(400).json({
@@ -68,7 +70,7 @@ const loginUser = async (req,res)=>{
       });
     }
     //user valid password or not
-    const isValidPssword = await User.comparePassword(password);
+    const isValidPssword = await user.comparePassword(password);
     if(!isValidPssword){
       logger.warn("Invalid password attempt", email);
       return res.status(400).json({
@@ -95,7 +97,79 @@ const loginUser = async (req,res)=>{
   }
 }
 //refresh token
+const refreshTokenController = async (req,res)=>{
+  logger.info("Refresh token endpoint hit");
+  try{
+    const { refreshToken } = req.body;
+    if(!refreshToken){
+      logger.warn("Refresh token not provided");
+      return res.status(400).json({
+        success: false,
+        message: "Refresh token is required",
+      });
+    }
+    const storedToken = await RefreshToken.findOne({token:refreshToken});
 
+    if(!storedToken || storedToken.expiryDate < new Date()){
+      logger.warn("Invalid refresh token");
+      return res.status(400).json({
+        success: false,
+        message: "Invalid refresh token",
+      });
+    }
+    const user = await User.findById(storedToken.user);
+    if(!user){
+      logger.warn("User not found for the given refresh token");
+      return res.status(400).json({
+        success: false,
+        message: "Invalid refresh token",
+      });
+    }
+    const {accessToken: newAccessToken, refreshToken: newRefreshToken} = await generateToken(user);
+    //delete old refresh token
+    await RefreshToken.deleteOne({_id:storedToken._id});
+    res.json({
+      success: true,
+      message: "Token refreshed successfully",
+      data: {
+        accessToken: newAccessToken,
+        refreshToken: newRefreshToken,
+      },
+    });
+
+   }catch(error){
+  logger.error("Error in refreshing token", error.message);
+  res.status(500).json({
+    success: false,
+    message: "Internal Server Error",
+  });
+}
+}
 //user logout
-
-module.exports = { registerUser , loginUser };
+const logoutUser = async (req,res)=>{
+  logger.info("Logout endpoint hit");
+  try{
+    const{refreshToken} = req.body;
+    if(!refreshToken){
+      logger.warn("Refresh token not provided for logout");
+      return res.status(400).json({
+        success: false,
+        message: "Refresh token is required",
+      });
+    }
+    logger.info("Deleting refresh token", refreshToken);
+    await RefreshToken.deleteOne({token:refreshToken});
+    logger.info("User logged out successfully");
+    res.status(200).json({
+      success: true,
+      message: "User logged out successfully",
+    });
+  }catch(error){
+    logger.error("Error in user logout", error.message);
+    res.status(500).json({
+      success: false,
+      message: "Internal Server Error",
+    });
+  }
+}
+module.exports = { registerUser , loginUser, refreshTokenController, logoutUser };
--- identity-services/routes/identity-service.js
+++ identity-services/routes/identity-service.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const router = express.Router();
 
-const {registerUser, loginUser} = require('../controllers/identity-controller');
+const {registerUser, loginUser, refreshTokenController, logoutUser} = require('../controllers/identity-controller');
 
 router.post('/register', registerUser);
 router.post('/login', loginUser);
+router.post('/refresh-token', refreshTokenController);
+router.post('/logout', logoutUser);
 
 module.exports = router;
